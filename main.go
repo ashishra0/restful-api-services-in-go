c892c3b274b82614b735b6615d8bea763427dca7
Ashish Rao <ashishrao2598@gmail.com>
2019-08-21 22:37:12 +0530

    add handler function

--- main.go
+++ main.go
@@ -6,7 +6,18 @@ import (
 	"os"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Page not found"))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("API version 1.0\n"))
+	}
+}
+
 func main() {
+	http.HandleFunc("/", rootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println(err)
