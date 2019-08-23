241cb6b321655640ecd3caaf79eaf1f064f78419
Ashish Rao <ashishrao2598@gmail.com>
2019-08-23 10:22:26 +0530

    add routes

--- handlers/usersRouter.go
+++ handlers/usersRouter.go
@@ -9,18 +9,22 @@ import (
 
 // UsersRouter handles the users route.
 func UsersRouter(w http.ResponseWriter, r *http.Request) {
+	// For the user list
 	path := strings.TrimSuffix(r.URL.Path, "/")
 	if path == "/users" {
 		switch r.Method {
 		case http.MethodGet:
+			getUsers(w, r)
 			return
 		case http.MethodPost:
+			createUser(w, r)
 			return
 		default:
 			postError(w, http.StatusMethodNotAllowed)
 			return
 		}
 	}
+	// For individual user
 	path = strings.TrimPrefix(path, "/users/")
 	if !bson.IsObjectIdHex(path) {
 		postError(w, http.StatusNotFound)
